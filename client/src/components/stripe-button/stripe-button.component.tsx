99231a8ec43e62d172fd013802de5a51d2937cb8
Lalo Vene <lalohdez77@gmail.com>
2022-01-01 21:36:18 -0600

    Added stripe backend

--- client/src/components/stripe-button/stripe-button.component.tsx
+++ client/src/components/stripe-button/stripe-button.component.tsx
@@ -1,13 +1,41 @@
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { useIonAlert } from "@ionic/react";
 
 const StripeCheckoutButton = ({ price }: { price: number }) => {
+  const [present] = useIonAlert();
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51JWiAqKWdvWiOub0w375wxqMtw4c4pmlfbeOfHv1RTfurS6DldrdXuWyvp6OGxKWUfovBmTa4fbaPzXqVa5OHk0g00boe8fEL7";
 
+  const presentAlert = (header: string, message: string) => {
+    present({
+      header,
+      message,
+      buttons: ["Ok"],
+    });
+  };
+
   const onToken = (token: any) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    }).then(
+      (response) => {
+        presentAlert("Success", "Payment Successful");
+      },
+      (error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        presentAlert(
+          "Error",
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      }
+    );
   };
   return (
     <StripeCheckout
